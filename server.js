ed5ba42950f2c398ba784b3bc70707553d7425bf
Glitch (jrbartone-a4-creativecoding) <none>
2019-09-21 14:24:38 +0000

    ⚡️😘 Checkpoint ./js/physi.js:6267243/4 ./index.html:6267243/118 ./server.js:6267243/576

--- server.js
+++ server.js
@@ -2,6 +2,21 @@
 const express = require('express');
 const app = express();
 app.use(express.static('public'));
+//app.use('/js', express.static('js'));
+
+
+
+app.get('/', function(request, response) {
+  response.sendFile(__dirname + '/index.html');
+});
+
+app.get('/js/three.js', function(request, response) {
+  response.sendFile(__dirname + '/js/three.js');
+});
+
+app.get('/js/physi.js', function(request, response) {
+  response.sendFile(__dirname + '/js/physi.js');
+});
 
 app.get('/', function(request, response) {
   response.sendFile(__dirname + '/index.html');
