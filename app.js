95ab47f6ecab575a9b2aed6ac5b0247280bba21f
René Borner <mail@reneborner.de>
2019-09-22 15:19:19 -0400

    added tree diagram

--- app.js
+++ app.js
@@ -1,22 +1,9 @@
-const Koa = require('koa')
-const app = new Koa()
+const Koa = require('koa'),
+    helmet = require("koa-helmet"),
+    compress = require('koa-compress'),
+    app = new Koa()
 
+app.use(compress())
+app.use(helmet())
 app.use(require('koa-static')("public"))
-app.use(async (ctx, next) => {
-    await next()
-    const rt = ctx.response.get('X-Response-Time')
-    console.log(`${ctx.method} ${ctx.url} - ${rt}`)
-})
-
-// x-response-time
-
-app.use(async (ctx, next) => {
-    const start = Date.now()
-    await next()
-    const ms = Date.now() - start
-    ctx.set('X-Response-Time', `${ms}ms`)
-})
-
-
-
 app.listen(3000)
