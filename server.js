24c8cf646b681b4aa349d3d4604e03aef854d992
Glitch (jrbartone-a4-creativecoding) <none>
2019-09-24 16:50:35 +0000

    🙎💧 Checkpoint ./index.html:6267243/1572 ./server.js:6267243/33

--- server.js
+++ server.js
@@ -2,6 +2,7 @@
 const express = require('express');
 const app = express();
 app.use(express.static(__dirname + '/public'));
+//let dat = require('dat.gui')
 //app.use('/js', express.static('js'));
 
 
