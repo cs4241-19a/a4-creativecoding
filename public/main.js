1ad0355c6135e9aa42191b598a0ce79b0d2450fb
René Borner <mail@reneborner.de>
2019-09-24 18:35:25 -0400

    added interactive family tree creation

--- public/main.js
+++ public/main.js
@@ -1,78 +1,195 @@
-const margin = {top: 20, right: 120, bottom: 20, left: 120},
-    width = 960 - margin.right - margin.left,
-    height = 500 - margin.top - margin.bottom
+function createTrumpTree() {
+    createTree([{
+        name: "Fred Trump",
+        class: "node",
+        textClass: "nodeText",
+        depthOffset: 1,
+        marriages: [{
+            spouse: {
+                name: "Mary Anne Trump",
+            },
+            children: [
+                {
+                    name: "Fred Trump Jr."
+                },
+                {
+                    name: "Elizabeth Trump Grau"
+                },
+                {
+                    name: "Donald Trump",
+                    class: "node",
+                    textClass: "nodeText",
+                    depthOffset: 1,
+                    marriages: [{
+                        spouse: {
+                            name: "Ivana Trump",
+                        },
+                        children: [{
+                            name: "Donald Trump Jr.",
+                        },
+                            {
+                                name: "Ivanka Trump",
+                            },
+                            {
+                                name: "Eric Trump",
+                            }]
+                    },
+                        {
+                            spouse: {
+                                name: "Marla Maples",
+                            },
+                            children: [{
+                                name: "Tiffany Trump",
+                            }
+                            ],
+                        },
+                        {
+                            spouse: {
+                                name: "Melania Knauss-Trump",
+                            },
+                            children: [{
+                                name: "Barron Trump",
+                            }
+                            ],
+                        }]
+                },
+                {
+                    name: "Maryanne Trump Barry",
+                },
+                {
+                    name: "Robert Trump"
+                }
+            ]
+        }],
+        extra: {}
+    }])
+}
 
-let i = 0
-
-const tree = d3.layout.tree()
-    .size([height, width])
-
-const diagonal = d3.svg.diagonal()
-    .projection(function (d) {
-        return [d.y, d.x]
+function createGUI() {
+    let adjustableValues = {
+        strokeColor: "black",
+        marriageStrokeColor: "green",
+        nodeBackground: "white",
+        fontColor: "black"
+    }
+    let gui = new dat.GUI()
+    let colors = gui.addFolder("Colors")
+    colors.add(adjustableValues, 'strokeColor', ['red', 'blue', 'green', 'white', 'black']).onChange(function (newValue) {
+        $(".linage").css({"stroke": newValue})
     })
-
-const svg = d3.select("body").append("svg")
-    .attr("width", width + margin.right + margin.left)
-    .attr("height", height + margin.top + margin.bottom)
-    .append("g")
-    .attr("transform", "translate(" + margin.left + "," + margin.top + ")")
-
-// load the external data
-d3.json("treeData.json", function (error, treeData) {
-    root = treeData
-    update(root)
-})
-
-function update(source) {
-
-    // Compute the new tree layout.
-    const nodes = tree.nodes(root).reverse(),
-        links = tree.links(nodes)
-
-    // Normalize for fixed-depth.
-    nodes.forEach(function (d) {
-        d.y = d.depth * 180
+    colors.add(adjustableValues, 'marriageStrokeColor', ['red', 'blue', 'green', 'white', 'black']).onChange(function (newValue) {
+        $(".marriage").css({"stroke": newValue})
     })
+    colors.add(adjustableValues, 'nodeBackground', ['red', 'blue', 'green', 'white', 'black']).onChange(function (newValue) {
+        $(".node").css({"background-color": newValue})
+    })
+    colors.add(adjustableValues, 'fontColor', ['red', 'blue', 'green', 'white', 'black']).onChange(function (newValue) {
+        $(".nodeText").css({"color": newValue})
+    })
+}
 
-    // Declare the nodes…
-    const node = svg.selectAll("g.node")
-        .data(nodes, function (d) {
-            return d.id || (d.id = ++i)
-        })
+function createTree(familyData) {
+    $("#instructions").remove()
+    // todo use two different es6 modules
+    //todo use dat.gui to zoom and change offsets (at least six parameters??)
+    let data = familyData
+    let options = {
+        target: '#graph',
+        debug: true,
+        callbacks: {
+            nodeRenderer: function (name, x, y, height, width, extra, id, nodeClass, textClass, textRenderer) {
+                let node = ''
+                node += '<div '
+                node += 'style="height:100%;width:100%;" '
+                node += 'class="' + nodeClass + ' btn btn-light" '
+                node += 'id="node' + id + '">\n'
+                node += textRenderer(name, extra, textClass)
+                node += '</div>'
+                return node
+            }
+        },
+        margin: {
+            top: 0,
+            right: 0,
+            bottom: 0,
+            left: 0
+        },
+        nodeWidth: 100,
+        styles: {
+            node: 'node',
+            linage: 'linage',
+            marriage: 'marriage',
+            text: 'nodeText'
+        }
+    }
+    dTree.init(data, options)
+    $('svg').removeAttr('width').removeAttr('height')
+    $('svg').each(function () {
+        $(this)[0].setAttribute('width', '100%')
+    })
+    $('svg').each(function () {
+        $(this)[0].setAttribute('viewBox', '0 0 600 600')
+    })
+    createGUI()
+}
 
-    // Enter the nodes.
-    const nodeEnter = node.enter().append("g")
-        .attr("class", "node")
-        .attr("transform", function (d) {
-            return "translate(" + d.y + "," + d.x + ")"
-        })
+function createCustomTree() {
+    $("#instructions").remove()
+    $form = $("<form>\n" +
+        "            <div class=\"form-row\">\n" +
+        "                <div class=\"form-group col-5\">\n" +
+        "                    <input id='name' type=\"text\" class=\"form-control\" placeholder=\"Name\">\n" +
+        "                </div>\n" +
+        "                <div class=\"form-group col-5\">\n" +
+        "                    <select id='relation' class=\"form-control\">\n" +
+        "                        <option>Children</option>\n" +
+        "                        <option>Spouse</option>\n" +
+        "                    </select>\n" +
+        "                </div>\n" +
+        "                <div class=\"form-group col-2\">\n" +
+        "                    <button id='formButton' class=\"btn btn-primary\">Add</button>\n" +
+        "                </div>\n" +
+        "             </div>\n" +
+        "        </form>")
+    $('#customForm').append($form)
+    $('#formButton').click(function () {
+        handleForm($('#name').val(), $("#relation option:selected").text())
+    })
+}
 
-    nodeEnter.append("circle")
-        .attr("r", 10)
-        .style("fill", "#fff")
+let data = []
+let children = 0
 
-    nodeEnter.append("text")
-        .attr("x", function (d) {
-            return d.children || d._children ? -13 : 13
-        })
-        .attr("dy", ".35em")
-        .attr("text-anchor", function (d) {
-            return d.children || d._children ? "end" : "start"
-        })
-        .text(function (d) {
-            return d.name
-        })
-        .style("fill-opacity", 1)
-
-    // Declare the links…
-    const link = svg.selectAll("path.link")
-        .data(links, function (d) {
-            return d.target.id
-        })
-
-    // Enter the links.
-    link.enter().insert("path", "g")
-        .attr("class", "link")
-        .attr("d", diagonal)
+function handleForm(name, relation) {
+    console.log(name + " " + relation)
+    if (!data.length > 0) {
+        data[0] = {
+            name: name,
+            class: "node",
+            textClass: "nodeText",
+            depthOffset: 1,
+            marriages: []
+        }
+        createTree(data)
+    } else {
+        if (relation === "Spouse") {
+            data[0].marriages[0] = {
+                spouse: {
+                    name: name,
+                },
+                children: []
+            }
+        } else if (relation === "Children") {
+            data[0].marriages[0].children[children++] = {
+                name: name,
+                class: "node",
+                textClass: "nodeText",
+                depthOffset: 1,
+                marriages: []
+            }
+        }
+        $("svg").remove()
+        $(".main").remove()
+        createTree(data)
+    }
 }
