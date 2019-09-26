31093ce381ba2f8ab488095e90e14e0fb5ab8c98
Hunter <huntercaouette@gmail.com>
2019-09-26 19:01:40 -0400

    adding ytdl (youtube downloader) to server to process link to yt vid

--- index.js
+++ index.js
@@ -13,6 +13,11 @@ const express   = require( 'express' ),
       // db        = low(adapter),
       helmet = require('helmet'),
       compression = require('compression'),
+      fs = require('fs'),
+      ytdl = require('ytdl-core'),
+      // socketIO = require('socket.io'),
+      // server = http.Server(app),
+      // io = socketIO(server),
       port      = 3000
 
 
@@ -23,8 +28,25 @@ app.use( bodyParser.json())
 app.use(favicon(path.join('public','res','favicon.ico')))
 app.use( session({ secret:'fromage', name:'a3-cookie', resave:false, saveUninitialized:true }) )
 // app.use(babel)
+app.use(morgan('dev'))
 
 app.get(('/' || '/index.html'), (req, res) => res.sendFile(public/index.html))
+
+
+app.post('/stream/:videoId', (request, response) => {
+  const videoId = request.params.videoId
+  const videoUrl = `https://www.youtube.com/watch?v=${videoId}`
+  if (!ytdl.validateURL(videoUrl)) {
+    response.send({ success: false })
+  } else {
+    ytdl(videoUrl, { filter: format => format.container === 'mp4' })
+      .pipe(response)
+  }
+})
+
+app.use((request, response) => {
+  response.redirect('/')
+})
 
 
 app.listen(port, () => console.log(`a3-hcaouette listening on port ${port}!`))
