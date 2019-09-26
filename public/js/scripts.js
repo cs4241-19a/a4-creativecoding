551352ecb37a079a2e292076673b00591061022c
Hunter <huntercaouette@gmail.com>
2019-09-25 22:26:12 -0400

    stripping a3 down to build a4

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -1,26 +1,3 @@
 // Add some Javascript code here, to run on the front end.
 
 console.log("Welcome to assignment 3!")
-
-const addToCart = function(chz){
-  console.log('attempting addToCart')
-  let body=JSON.stringify({ cheese: chz})
-
-  fetch( '/addToCart', {
-    method:'POST',
-    body,
-    credentials: 'include',
-    headers: { 'Content-Type': 'application/json' }
-  })
-  .then( res => res.json() )
-  .then( console.log )
-}
-
-const loadAcct = function(chz){
-  fetch( '/loadAcct', {
-    method:'GET',
-    credentials: 'include',
-  })
-  .then( res => res.json() )
-  .then( console.log )
-}
