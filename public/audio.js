fbbf4d4487fbf06fcf60249d3fe1e10effde273e
Evan Goldstein <ebgoldstein@wpi.edu>
2019-09-26 11:43:02 -0400

    Circle now a class!

--- public/audio.js
+++ public/audio.js
@@ -44,12 +44,6 @@ let barHeight;
 let x = 0;
 
 
-function renderFrame() {
-    requestAnimationFrame(renderFrame);
-    bar();
-    circle();
-}
-
 function bar() {
     x = 0;
     analyser.getByteFrequencyData(frequencyData);
@@ -90,55 +84,59 @@ function bar() {
 //     },
 //     false);
 
-function circle() {
-    // set to the size of device
-    let center_x = canvas.width / 2;
-    let center_y = canvas.height / 2;
-    const radius = 150;
+class circle {
+    constructor(low, high) {
+        this.low = low;
+        this.high = high;
+        this.rads = Math.PI * 2 / (this.high - this.low);
+        this.center_x = canvas.width / 2;
+        this.center_y = canvas.height / 2;
+        this.radius = 150;
+    }
 
-    let gradient = ctx.createLinearGradient(0, 0, 0, canvas.height);
-    gradient.addColorStop(0, "rgba(35, 7, 77, 1)");
-    gradient.addColorStop(1, "rgba(204, 83, 51, 1)");
-    // ctx.fillStyle = gradient;
-    // ctx.fillRect(0,0,canvas.width,canvas.height);
+    draw() {
+        let gradient = ctx.createLinearGradient(0, 0, 0, canvas.height);
+        gradient.addColorStop(0, "rgba(35, 7, 77, 1)");
+        gradient.addColorStop(1, "rgba(204, 83, 51, 1)");
+        // ctx.fillStyle = gradient;
+        // ctx.fillRect(0,0,canvas.width,canvas.height);
 
-    //draw a circle
-    ctx.beginPath();
-    ctx.arc(center_x, center_y, radius, 0, 2 * Math.PI);
-    ctx.stroke();
+        ctx.beginPath();
+        ctx.arc(this.center_x, this.center_y, this.radius, 0, 2 * Math.PI);
+        ctx.stroke();
 
-    analyser.getByteFrequencyData(frequencyData);
-    let low = bufferLength * .50;
-    let high = bufferLength * 0.75;
-    for (let i = low; i < high; i++) {
+        analyser.getByteFrequencyData(frequencyData);
+        for (let i = this.low; i < this.high; i++) {
+            this.drawBar(i);
+        }
+    }
 
-        let rads = Math.PI * 2 / (high - low);
-
+    drawBar(i) {
+        let frequency = frequencyData[i];
         let bar_height = frequencyData[i] * 0.7;
-        const cos = Math.cos(rads * i);
-        const sin = Math.sin(rads * i);
-        let x = center_x + cos * radius;
-        let y = center_y + sin * radius;
-        let x_end = center_x + cos * (radius + bar_height);
-        let y_end = center_y + sin * (radius + bar_height);
+        const cos = Math.cos(this.rads * i);
+        const sin = Math.sin(this.rads * i);
+        let x = this.center_x + cos * this.radius;
+        let y = this.center_y + sin * this.radius;
+        let x_end = this.center_x + cos * (this.radius + bar_height);
+        let y_end =this. center_y + sin * (this.radius + bar_height);
 
-        drawBar(x, y, x_end, y_end, circleBarWidth, frequencyData[i]);
-
+        ctx.strokeStyle = "rgb(" + frequency + ", " + frequency + ", " + 205 + ")";
+        ctx.lineWidth = barWidth;
+        ctx.beginPath();
+        ctx.moveTo(x, y);
+        ctx.lineTo(x_end, y_end);
+        ctx.stroke();
     }
 }
 
-function drawBar(x1, y1, x2, y2, width, frequency) {
 
-    var lineColor = "rgb(" + frequency + ", " + frequency + ", " + 205 + ")";
+let circle1 = new circle(bufferLength * .50, bufferLength * .75);
 
-    ctx.strokeStyle = lineColor;
-    ctx.lineWidth = width;
-    ctx.beginPath();
-    ctx.moveTo(x1, y1);
-    ctx.lineTo(x2, y2);
-    ctx.stroke();
+function renderFrame() {
+    requestAnimationFrame(renderFrame);
+    bar();
+    circle1.draw();
 }
-
-// bar()
 renderFrame();
 audioContext.resume();
