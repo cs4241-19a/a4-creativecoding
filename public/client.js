ec7cada8c73ed4444ec2558c9f64b43384a75e35
ABon27 <drewbon.alt@gmail.com>
2019-09-25 23:54:42 -0400

    can roll some 3d dice

--- public/client.js
+++ public/client.js
@@ -55,6 +55,7 @@ const submit = function( e ) {
                         drawDice(responseData.rolls, responseData.diceNum);
                     }, 3500);
                     roll3dDice.init(input2.value, "d6");
+                    window.scrollTo(0, 600);
                     //window.location = '/views/index.html#canvas';
                 });
 
@@ -106,8 +107,11 @@ const roll3dDice = {
 
             this.createLights();
             //this.knot = this.createKnot();
-            this.diceList = this.createD4();
+            //this.diceList = this.createD4();
             //this.diceList = this.createD6();
+            //this.diceList = this.createD8();
+            //this.diceList = this.createD10();
+            this.diceList = this.createD12();
 
             // ...the rare and elusive hard binding appears! but why?
             this.render = this.render.bind( this );
@@ -137,20 +141,17 @@ const roll3dDice = {
                 const geometry = new THREE.Geometry();
 
                 geometry.vertices = [
-                    new THREE.Vector3( 0, 0, 0 ),
-                    new THREE.Vector3( 0, 1, 0 ),
-                    new THREE.Vector3( 1, 1, 0 ),
-                    new THREE.Vector3( 1, 0, 0 ),
-                    new THREE.Vector3( 0.5, 0.5, 1 )
+                    new THREE.Vector3( 1, 1, 1 ),
+                    new THREE.Vector3( -1, -1, 1 ),
+                    new THREE.Vector3( -1, 1, -1 ),
+                    new THREE.Vector3(1, -1, -1)
                 ];
 
                 geometry.faces = [
-                    new THREE.Face3( 0, 1, 2 ),
-                    new THREE.Face3( 0, 2, 3 ),
-                    new THREE.Face3( 1, 0, 4 ),
-                    new THREE.Face3( 2, 1, 4 ),
-                    new THREE.Face3( 3, 2, 4 ),
-                    new THREE.Face3( 0, 3, 4 )
+                    new THREE.Face3( 1, 0, 2, 1 ),
+                    new THREE.Face3( 0, 1, 3, 2 ),
+                    new THREE.Face3( 0, 3, 2, 3 ),
+                    new THREE.Face3( 1, 2, 3,4 )
                 ];
                 const transformation = new THREE.Matrix4().makeScale(1, 1, 1);
                 geometry.applyMatrix( transformation );
@@ -182,9 +183,156 @@ const roll3dDice = {
             return diceList;
         },
 
-        randomPlacement(start, end){
-            return Math.floor(Math.random() * (end - start)) + start;
+        createD8(){
+            let diceList = [];
+            for(let i = 0; i < this.number; i++){
+                const geometry = new THREE.Geometry();
+
+                geometry.vertices = [
+                    new THREE.Vector3( 1, 0, 0 ),
+                    new THREE.Vector3( -1, 0, 0 ),
+                    new THREE.Vector3( 0, 1, 0 ),
+                    new THREE.Vector3(0, -1, 0),
+                    new THREE.Vector3(0, 0, 1),
+                    new THREE.Vector3(0, 0, -1)
+                ];
+
+                geometry.faces = [
+                    new THREE.Face3( 0, 2, 4, 1 ),
+                    new THREE.Face3( 0, 4, 3, 2 ),
+                    new THREE.Face3( 0, 3, 5, 3 ),
+                    new THREE.Face3( 0, 5, 2, 4 ),
+                    new THREE.Face3( 1, 3, 4, 5 ),
+                    new THREE.Face3( 1, 4, 2, 6),
+                    new THREE.Face3( 1, 2, 5, 7),
+                    new THREE.Face3( 1, 5, 3, 8)
+                ];
+                const transformation = new THREE.Matrix4().makeScale(1, 1, 1);
+                geometry.applyMatrix( transformation );
+                const material = new THREE.MeshBasicMaterial({color: 0xff0000, shininess:2000});
+                const d8 = new THREE.Mesh(geometry, material);
+                diceList.push(d8);
+                //let v = new THREE.Vector3(this.randomPlacement(0, window.innerWidth), this.randomPlacement(0, window.innerHeight), this.randomPlacement(-100, 100));
+                console.log(diceList[i]);
+                //diceList[i].position.set(v);
+                this.scene.add(diceList[i]);
+            }
+
+            return diceList;
         },
+
+        createD10(){
+            let diceList = [];
+            for(let i = 0; i < this.number; i++){
+                const geometry = new THREE.Geometry();
+
+                this.vertices = [];
+
+                geometry.faces = [
+                    new THREE.Face3( 5, 7, 11, 0 ),
+                    new THREE.Face3( 4, 2, 10, 11 ),
+                    new THREE.Face3( 1, 3, 11, 2 ),
+                    new THREE.Face3( 0, 8, 10, 3 ),
+                    new THREE.Face3( 7, 9, 11, 4 ),
+                    new THREE.Face3( 8, 6, 10, 5 ),
+                    new THREE.Face3( 9, 1, 11, 6 ),
+                    new THREE.Face3( 2, 0, 10, 7 ),
+                    new THREE.Face3( 3, 5, 11, 8 ),
+                    new THREE.Face3( 6, 4, 10, 9 ),
+                    new THREE.Face3( 1, 0, 2, -1 ),
+                    new THREE.Face3( 1, 2, 3, -1 ),
+                    new THREE.Face3( 3, 2, 4, -1 ),
+                    new THREE.Face3( 3, 4, 5, -1 ),
+                    new THREE.Face3( 5, 4, 6, -1 ),
+                    new THREE.Face3( 5, 6, 7, -1 ),
+                    new THREE.Face3( 7, 6, 8, -1 ),
+                    new THREE.Face3( 7, 8, 9, -1 ),
+                    new THREE.Face3( 9, 8, 0, -1 ),
+                    new THREE.Face3( 9, 0, 1, -1 )
+                ];
+                for (let i = 0, b = 0; i < 10; ++i, b += Math.PI * 2 / 10) {
+                    this.vertices.push([Math.cos(b), Math.sin(b), 0.105 * (i % 2 ? 1 : -1)]);
+                }
+                this.vertices.push([0, 0, -1]);
+                this.vertices.push([0, 0, 1]);
+                geometry.vertices = this.vertices;
+                const transformation = new THREE.Matrix4().makeScale(1, 1, 1);
+                geometry.applyMatrix( transformation );
+                const material = new THREE.MeshBasicMaterial({color: 0xff0000, shininess:2000});
+                const d10 = new THREE.Mesh(geometry, material);
+                diceList.push(d10);
+                //let v = new THREE.Vector3(this.randomPlacement(0, window.innerWidth), this.randomPlacement(0, window.innerHeight), this.randomPlacement(-100, 100));
+                console.log(diceList[i]);
+                //diceList[i].position.set(v);
+                this.scene.add(diceList[i]);
+            }
+
+            return diceList;
+        },
+
+        createD12(){
+            let diceList = [];
+            for(let i = 0; i < this.number; i++){
+                const geometry = new THREE.Geometry();
+
+                let p = (1 + Math.sqrt(5)) / 2;
+                let q = 1 / p;
+                this.vertices = [
+                    new THREE.Vector3( 0, q, p ),
+                    new THREE.Vector3( 0, q, -p ),
+                    new THREE.Vector3( 0, -q, p ),
+                    new THREE.Vector3( 0, -q, -p ),
+                    new THREE.Vector3( p, 0, q ),
+                    new THREE.Vector3( p, 0, -q ),
+                    new THREE.Vector3( -p, 0, q ),
+                    new THREE.Vector3( -p, 0, -q ),
+                    new THREE.Vector3( q, p, 0 ),
+                    new THREE.Vector3( q, -p, 0 ),
+                    new THREE.Vector3( -q, p, 0 ),
+                    new THREE.Vector3( -q, -p, 0 ),
+                    new THREE.Vector3( 1, 1, 1 ),
+                    new THREE.Vector3( 1, 1, -1 ),
+                    new THREE.Vector3( 1, -1, 1 ),
+                    new THREE.Vector3( 1, -1, -1 ),
+                    new THREE.Vector3( -1, 1, 1 ),
+                    new THREE.Vector3( -1, 1, -1 ),
+                    new THREE.Vector3( -1, -1, 1 ),
+                    new THREE.Vector3( -1, -1, -1 )
+
+                ];
+
+                geometry.faces = [
+                    new THREE.Face3( 2, 14, 4, 12, 0, 1 ),
+                    new THREE.Face3( 15, 9, 11, 19, 3, 2 ),
+                    new THREE.Face3( 16, 10, 17, 7, 6, 3 ),
+                    new THREE.Face3( 6, 7, 19, 11, 18, 4 ),
+                    new THREE.Face3( 6, 18, 2, 0, 16, 5 ),
+                    new THREE.Face3( 18, 11, 9, 14, 2, 6 ),
+                    new THREE.Face3( 1, 17, 10, 8, 13, 7 ),
+                    new THREE.Face3( 1, 13, 5, 15, 3, 8 ),
+                    new THREE.Face3( 13, 8, 12, 4, 5, 9 ),
+                    new THREE.Face3( 5, 4, 14, 9, 15, 10 ),
+                    new THREE.Face3( 0, 12, 8, 10, 16, 11 ),
+                    new THREE.Face3( 3, 19, 7, 17, 1, 12 )
+                ];
+
+                const transformation = new THREE.Matrix4().makeScale(1, 1, 1);
+                geometry.applyMatrix( transformation );
+                const material = new THREE.MeshBasicMaterial({color: 0xff0000, shininess:2000});
+                const d12 = new THREE.Mesh(geometry, material);
+                diceList.push(d12);
+                //let v = new THREE.Vector3(this.randomPlacement(0, window.innerWidth), this.randomPlacement(0, window.innerHeight), this.randomPlacement(-100, 100));
+                console.log(diceList[i]);
+                //diceList[i].position.set(v);
+                this.scene.add(diceList[i]);
+            }
+
+            return diceList;
+        },
+
+        randomPlacement(start, end) {
+            return Math.floor(Math.random() * (end - start)) + start;
+            },
 
         render() {
             //this.knot.rotation.x += .025;
