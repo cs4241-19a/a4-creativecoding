74454780d6ca2620b7b80e5eee647313df401872
Hunter <huntercaouette@gmail.com>
2019-09-26 23:40:35 -0400

    js

--- index.js
+++ index.js
@@ -1,16 +1,12 @@
 const express   = require( 'express' ),
       app       = express(),
       morgan    = require( 'morgan' )
-      session   = require( 'express-session' ),           //1
-      // passport  = require( 'passport' ),                  //2
-      // GoogleStrategy = require('passport-google-oauth').OAuth2Strategy;
-      bodyParser= require( 'body-parser' ),               //3
-      favicon   = require( 'serve-favicon' ),             //4
+      // session   = require( 'express-session' ),
+      // passport  = require( 'passport' ),
+      // GoogleStrategy = require('passport-google-oauth').OAuth2Strategy,
+      bodyParser= require( 'body-parser' ),
+      favicon   = require( 'serve-favicon' ),
       path      = require( 'path' ),
-      // low       = require('lowdb'),                       //5
-      // FileSync  = require('lowdb/adapters/FileSync'),
-      // adapter   = new FileSync('db.json'),
-      // db        = low(adapter),
       helmet = require('helmet'),
       compression = require('compression'),
       fs = require('fs'),
@@ -26,7 +22,7 @@ app.use(compression())
 app.use( express.static('public'))
 app.use( bodyParser.json())
 app.use(favicon(path.join('public','res','favicon.ico')))
-app.use( session({ secret:'fromage', name:'a3-cookie', resave:false, saveUninitialized:true }) )
+// app.use( session({ secret:'fromage', name:'a3-cookie', resave:false, saveUninitialized:true }) )
 // app.use(babel)
 app.use(morgan('dev'))
 
