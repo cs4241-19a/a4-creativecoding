44dab149cdd1bafa74712ba85ad0bbf6c934e0de
Joseph Yuen <josephnewbox@gmail.com>
2019-09-27 18:42:12 -0400

    added in showing/hiding documentation and canvas arena (still need to implement tho)

--- public/js/main.js
+++ public/js/main.js
@@ -1,4 +1,73 @@
 import * as mod1 from './module1.js'
 import * as mod2 from './module2.js'
 
+var startBtn = document.getElementById('startBtn')
+var titleTxt = document.getElementById('titleTxt')
+var directionsTitleTxt = document.getElementById('directionsTitleTxt')
+var directionsTxt = document.getElementById('directionsTxt')
+
+var tutorialBtn = document.getElementById('tutorialBtn')
+var closeBtn = document.getElementById('closeBtn')
+
+const gameCanvas = document.getElementById('gameCanvas')
+const ctx = gameCanvas.getContext('2d')
+
+// ctx style test
+ctx.fillStyle = 'green'
+ctx.fillRect(10, 10, 150, 100)
+
+window.onload = function () {
+  const startBtn = document.querySelector('#startBtn')
+  startBtn.onclick = startgame
+  const tutorialBtn = document.querySelector('#tutorialBtn')
+  tutorialBtn.onclick = showDocumentation
+  const closeBtn = document.querySelector('#closeBtn')
+  closeBtn.onclick = hideDocumentation
+}
+
+const startgame = function (e) {
+  e.preventDefault()
+
+  console.log('Starting game')
+  titleTxt.style.display = 'none'
+  directionsTitleTxt.style.display = 'none'
+  directionsTxt.style.display = 'none'
+  startBtn.style.display = 'none'
+  tutorialBtn.style.display = 'block'
+
+  gameCanvas.style.display = 'block'
+  // start game
+}
+
+const showDocumentation = function (e) {
+  e.preventDefault()
+
+  console.log('Showing Documentation')
+  titleTxt.style.display = 'block'
+  directionsTitleTxt.style.display = 'block'
+  directionsTxt.style.display = 'block'
+  tutorialBtn.style.display = 'none'
+  closeBtn.style.display = 'block'
+
+  gameCanvas.style.display = 'none'
+  // pause game
+}
+
+const hideDocumentation = function (e) {
+  e.preventDefault()
+
+  console.log('Hiding Documentation')
+  titleTxt.style.display = 'none'
+  directionsTitleTxt.style.display = 'none'
+  directionsTxt.style.display = 'none'
+  tutorialBtn.style.display = 'block'
+  closeBtn.style.display = 'none'
+
+  gameCanvas.style.display = 'block'
+  // unpause game
+}
+
+mod1.hello()
+mod2.hello()
+
 console.log('main.js')
--- public/js/module1.js
+++ public/js/module1.js
@@ -1,4 +1,33 @@
 console.log('module1.js')
 
+function start () {
+  arena.start()
+}
+
+// Canvas
+var arena = {
+  canvas: document.createElement('canvas'),
+  start: function () {
+    this.canvas.width = 480
+    this.canvas.height = 270
+    this.context = this.canvas.getContext('2d')
+    document.body.insertBefore(this.canvas, document.body.childNodes[0])
+    this.interval = setInterval(updateArena, 20)
+  },
+  clear: function () {
+    this.context.clearRect(0, 0, this.canvas.width, this.canvas.height)
+  }
+}
+
+function updateArena () {
+  arena.clear()
+  arena.newPos()
+  arena.update()
+}
+
+function hello () {
+  console.log('hello from module 1')
+}
+
 // Export functions and const
-export {}
+export { start, hello }
