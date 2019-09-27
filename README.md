5090a048ec4e17efd197a557bc059ce6caa93098
Andrew <armorrison@wpi.edu>
2019-09-27 18:59:44 -0400

    Finished

--- README.md
+++ README.md
@@ -1,48 +1,20 @@
-Assignment 4 - Creative Coding: Interactive Multimedia Experiences
-===
-
-Due: September 27th, by 11:59 PM.
-
-For this assignment we will focus on client-side development using popular audio/graphics/visualization technologies; the server requirements are minimal. The goal of this assignment is to refine our JavaScript knowledge while exploring the multimedia capabilities of the browser.
-
-Baseline Requirements
----
-
-Your application is required to implement the following functionalities:
-
-
-- A user interface for interaction with your project, which must expose at least six parameters for user control. [dat.gui](https://workshop.chromeexperiments.com/examples/gui/#1--Basic-Usage) is highly recommended for this. You might also explore interaction by tracking mouse movement via the `window.onmousemove` event handler in tandem with the `event.clientX` and `event.clientY` properties. Consider using the [Pointer Events API](https://developer.mozilla.org/en-US/docs/Web/API/Pointer_events) to ensure that that mouse and touch events will both be supported in your app.
-- Your application should feature at least two different ES6 modules that you write ([read about ES6 modules](https://www.sitepoint.com/understanding-es6-modules/)) and include into a main JavaScript file. This means that you will need to author *at least three JavaScript files* (a `app.js` or `main.js` file and two modules). We'll discuss modules in class on Monday 9/23; for this assignment modules should contain at least two functions.
-- Your HTML and CSS should validate. There are options/plugins for most IDEs to check validation.
-
-Deliverables
----
-
-Do the following to complete this assignment:
-
-1. Implement your project with the above requirements.
-3. Test your project to make sure that when someone goes to your main page on Glitch/Heroku/etc., it displays correctly.
-4. Ensure that your project has the proper naming scheme `a4-yourname` so we can find it.
-5. Fork this repository and modify the README to the specifications below. *NOTE: If you don't use Glitch for hosting (where we can see the files) then you must include all project files that you author in your repo for this assignment*.
-6. Create and submit a Pull Request to the original repo. Name the pull request using the following template: `a4-gitname-firstname-lastname`.
-
-Sample Readme (delete the above when you're ready to submit, and modify the below so with your links and descriptions)
----
-
 ## Your Web Application Title
 
 your hosting link e.g. http://a4-charlieroberts.glitch.me
 
-Include a very brief summary of your project here. Images are encouraged, along with concise, high-level text. Be sure to include:
-
-- the goal of the application
-- challenges you faced in realizing the application
-- a brief description of the JS linter you used and what rules it follows (we'll be looking at your JS files for consistency)
+- The goal of this application was to compare video games sales in different countries, and see how certain games sold differently across the globe.
+- The hardest part was learning D3, since I have never used it before. I had to take several tutorials and had quite a few headaches getting even simple things to work correctly.
+- The JS Linter I used was ESLint, it follows the default rules. I used it mostly for checking for unused and nonexistant variables. I found it to be quite helpful when coding.
 
 ## Technical Achievements
-- **Tech Achievement 1**: I wrote my own custom GLSL shaders to use as a material for my Three.js objects.
+- **Tech Achievement 1**: I used a kaggle dataset from https://www.kaggle.com/sidtwr/videogames-sales-dataset that I transferred to a JSON format
+- **Tech Achievement 2**: I used D3 to create a chart and graphed the points from my dataset based on their sales and year of release
+- **Tech Achievement 3**: I used dat.GUI to edit my D3 visualization, which required redrawing certain parts of the graph as they changed
+- **Tech Achievement 4**: I developed a simple algorithm to map a sales value of a point to a RGB value that I used in my design
 
 ### Design/Evaluation Achievements
 - **Design Achievement 1**: I developed a custom color generator depending on the value of the node in the graph
 - **Design Achievement 2**: I created custom a custom axis for the year so it would only display Integers
+- **Design Achievement 3**: I added a hover feature that changes the opacity of points when the mouse is over them, and shows a tooltip of what game you are looking at as well as the sales for that game
+- **Design Achievement 2**: I went for a very clean design that had components that are fun and satisfying to mess with
 
--- public/js/main.js
+++ public/js/main.js
@@ -1,67 +1,15 @@
+import * as colorController from './module1.js';
+import * as clientLogic from './module2.js'
+
 var ps4data = [];
-var xboxdata = [];
 
 const closeHelper = function () {
   var helperText = document.getElementById("helper-text")
-  if (helperText.style.opacity == 0.0) {
-    helperText.style.opacity = 1
-  } else {
-    helperText.style.opacity = 0
-  }
+  colorController.setOpacity(helperText)
 }
 
-const fetchPS4Data = function () {
-  return new Promise(
-    function (resolve, reject) {
-      fetch("/ps4data", {
-        credentials: "include"
-      })
-        .then(response => response.json())
-        .then(data => {
-          ps4data = data
-          resolve("done")
-        })
-        .catch(err => {
-          reject("error")
-          console.log(err);
-        });
-    }
-  )
-
-};
-
-const fetchXboxData = function () {
-  return new Promise(
-    function (resolve, reject) {
-      fetch("/xboxdata", {
-        method: "GET",
-        credentials: "include"
-      })
-        .then(response => response.json())
-        .then(data => {
-          xboxdata = data;
-          resolve("done")
-        })
-        .catch(err => {
-          reject("error")
-          console.log(err);
-        });
-    }
-  )
-};
-
-
-
-// Takes in a value and returns a custom color based on the value
-const customColor = function (value) {
-  let r = Math.min(Math.pow(Math.round(value), 2) * 15, 255);
-  let g = 0;
-  let b = 0;
-  return `rgb(${r}, ${g}, ${b})`;
-};
-
 var div = d3
-  .select("body")
+  .select(".toolSection")
   .append("div")
   .attr("class", "tooltip")
   .text(function () {
@@ -81,6 +29,8 @@ var visualize = function () {
   this.border = "white"
   this.yValue = "Europe";
 
+  var vm = this // save this for later
+
   var svg = d3
     .select(".sales")
     .attr("width", this.svgWidth)
@@ -89,15 +39,11 @@ var visualize = function () {
 
   // The scale converts the data from the input domain, to an output range
   // These functions map an input domain to an output range. https://www.dashingd3js.com/d3js-scales
+  // Create x axis scale
   var xScale = d3
     .scaleLinear()
     .domain([this.minYear, this.maxYear])
     .range([0, this.svgWidth]);
-
-  var yScale = d3
-    .scaleLinear()
-    .domain([this.maxSales, this.minSales])
-    .range([0, this.svgHeight]);
 
   const xAxisTicks = xScale.ticks().filter(tick => Number.isInteger(tick));
 
@@ -106,20 +52,25 @@ var visualize = function () {
     .tickValues(xAxisTicks) // remove non-integer ticks
     .tickFormat(d3.format("d"));
 
-  var yAxis = d3.axisRight(yScale);
-
   svg
     .append("g")
     .attr("transform", `translate(0, ${this.svgHeight})`) // use backtick for template literals, very cool :D
     .call(xAxis);
+
+  // Create y scale
+  var yScale = d3
+    .scaleLinear()
+    .domain([this.maxSales, this.minSales])
+    .range([0, this.svgHeight]);
+
+  var yAxis = d3.axisRight(yScale);
 
   svg
     .append("g")
     .attr("transform", `translate(0, 0)`)
     .call(yAxis);
 
-  var vm = this
-
+  // Create bar chart
   var barChart = svg
     .selectAll("circle")
     .data(ps4data)
@@ -133,35 +84,32 @@ var visualize = function () {
     })
     .attr("r", this.circleSize + "px")
     .attr("fill", function (d) {
-      return customColor(d[vm.yValue]);
+      return colorController.customColor(d[vm.yValue]);
     })
-    .on("mouseover", function (d, i) {
+    .on("mouseover", function () {
       d3.select(this)
         .transition()
         .duration(50)
         .attr("opacity", .5);
-
-
       // Make the div appear with the game and value
       div
         .transition()
         .duration("50")
         .style("opacity", 1);
-
       // Assign the div the game and value
       div.html(d.Game + ": " + d[vm.yValue]);
     })
-    .on("mouseout", function (d, i) {
+    .on("mouseout", function () {
       d3.select(this)
         .transition()
         .duration(50)
         .attr("opacity", 1);
-
       div
         .transition()
         .duration("50")
         .style("opacity", 0);
     });
+
 
   this.updateCircleSize = function () {
     barChart.attr('r', this.circleSize + 'px')
@@ -169,51 +117,28 @@ var visualize = function () {
 
   this.updateYValue = function (value) {
     var country = document.getElementById('country')
-    if (value === "Japan")
-      vm.yValue = "Japan"
-    if (value === "Europe")
-      vm.yValue = "Europe"
-    if (value === "North America")
-      vm.yValue = "North America"
-    if (value === "Rest of World")
-      vm.yValue = "Rest of World"
-    if (value === "Global")
-      vm.yValue = "Global"
-    
+    vm.yValue = value
+
     country.innerHTML = value
     barChart.attr("cy", function (d) {
       return yScale(d[vm.yValue]);
     })
+      .attr("fill", function (d) {
+        return colorController.customColor(d[vm.yValue]);
+      })
   }
 
   this.updateMaxSales = function (value) {
     vm.maxSales = value
-    yScale = d3
-      .scaleLinear()
-      .domain([vm.maxSales, vm.minSales])
-      .range([0, vm.svgHeight]);
-
-    barChart
-      .attr("cy", function (d) {
-        return yScale(d[vm.yValue]);
-      })
-
-    yAxis = d3.axisRight(yScale);
-
-    svg.selectAll('g').remove()
-
-    svg.append('g')
-      .attr("transform", 'translate(0, 0)')
-      .call(yAxis);
-
-    svg
-      .append("g")
-      .attr("transform", `translate(0, ${this.svgHeight})`) // use backtick for template literals, very cool :D
-      .call(xAxis);
+    updateSales()
   }
 
   this.updateMinSales = function (value) {
     vm.minSales = value
+    updateSales()
+  }
+
+  const updateSales = function () {
     yScale = d3
       .scaleLinear()
       .domain([vm.maxSales, vm.minSales])
@@ -223,6 +148,7 @@ var visualize = function () {
       .attr("cy", function (d) {
         return yScale(d[vm.yValue]);
       })
+
 
     yAxis = d3.axisRight(yScale);
 
@@ -238,12 +164,12 @@ var visualize = function () {
       .call(xAxis);
   }
 
-  this.updateColor = function(value) {
+  this.updateColor = function (value) {
     vm.fill = value
     svg.attr("style", "background-color: " + vm.fill + "; border: 2px solid " + vm.border)
-  } 
+  }
 
-  this.updateBorder = function(value) {
+  this.updateBorder = function (value) {
     vm.border = value
     svg.attr("style", "border: 1px solid " + vm.border + "; background-color: " + vm.fill)
   }
@@ -251,16 +177,16 @@ var visualize = function () {
 
 window.onload = function () {
 
-  var helper = document.getElementById("helper").onclick = closeHelper
+  document.getElementById("helper").onclick = closeHelper
 
-  fetchPS4Data()
-    .then(fetchXboxData)
+  clientLogic.fetchPS4Data()
     .then(function () {
+      ps4data = clientLogic.getPS4Data
       var visuals = new visualize();
       var gui = new dat.GUI();
 
       var circleSize = gui.add(visuals, "circleSize").min(0).max(30);
-      circleSize.onChange(function (value) {
+      circleSize.onChange(function () {
         visuals.updateCircleSize();
       });
 
@@ -270,10 +196,10 @@ window.onload = function () {
       gui.add(visuals, "maxSales").min(2).max(20).onChange(function (value) {
         visuals.updateMaxSales(value)
       })
-      gui.add(visuals, "fill", ["white", "orange", "violet", "pink", "red", "black", "blue"]).onChange(function (value) {
+      gui.add(visuals, "fill", colorController.getColors()).onChange(function (value) {
         visuals.updateColor(value)
       })
-      gui.add(visuals, "border", ["white", "orange", "violet", "pink", "red", "black", "blue"]).onChange(function (value) {
+      gui.add(visuals, "border", colorController.getColors()).onChange(function (value) {
         visuals.updateBorder(value)
       })
       var yValue = gui.add(visuals, "yValue", ["Europe", "Japan", "North America", "Rest of World", "Global"]);
