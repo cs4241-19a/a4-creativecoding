5bd318294b99a85e015538e779c9b06be73fe0a5
Terry Hearst <thearst3rd@gmail.com>
2019-09-27 16:42:17 -0400

    BIG commit - browserify, standard, three all implemented

--- public/client.js
+++ public/client.js
@@ -1,43 +1,91 @@
-// client-side js
-// run by the browser each time your view template is loaded
+/*
+ * Client-side JS for CS 4241 Assignment 4
+ * by Terry Hearst
+ */
 
-console.log('hello world :o')
+const THREE = require('three')
 
-// our default array of dreams
-const dreams =
-[
-  'Find and count some sheep',
-  'Climb a really tall mountain',
-  'Wash the dishes'
-]
+// ###########################
+// ## SETUP THREE.JS CANVAS ##
+// ###########################
 
-// define variables that reference elements on our page
-const dreamsList = document.getElementById('dreams')
-const dreamsForm = document.forms[0]
-const dreamInput = dreamsForm.elements.dream
+const viewportElement = document.getElementById('viewport')
 
-// a helper function that creates a list item for a given dream
-const appendNewDream = function (dream) {
-  const newListItem = document.createElement('li')
-  newListItem.innerHTML = dream
-  dreamsList.appendChild(newListItem)
+const renderer = new THREE.WebGLRenderer()
+const canvas = renderer.domElement
+
+viewportElement.appendChild(canvas)
+
+const w = canvas.clientWidth
+const h = canvas.clientHeight
+
+console.log('CANVAS SIZE: ', w, h)
+
+renderer.setSize(w, h)
+
+// ###########################
+// ## SETUP ACTUAL 3D SCENE ##
+// ###########################
+
+// Setup Scene and Camera objs
+const scene = new THREE.Scene()
+const camera = new THREE.PerspectiveCamera(75, w / h, 0.1, 1000)
+
+camera.position.z = 10
+
+// Create knot
+const geo1 = new THREE.TorusKnotGeometry(1, 0.4, 64, 8, 2, 3)
+const mat1 = new THREE.MeshStandardMaterial({ color: 0x0000FF })
+const mesh1 = new THREE.Mesh(geo1, mat1)
+
+scene.add(mesh1)
+
+// Define lighting
+const ambLight = new THREE.AmbientLight(0x404040)
+const dirLight = new THREE.DirectionalLight(0xFFFFFF)
+
+scene.add(ambLight)
+scene.add(dirLight)
+
+// Grid
+var gridHelper = new THREE.GridHelper(10, 20)
+gridHelper.rotation.x = Math.PI / 2
+scene.add(gridHelper)
+
+// ##############################
+// ## MAIN RENDERING FUNCTIONS ##
+// ##############################
+
+// Checks to see if the canvas was resized and handles that properly
+function resize () {
+  const width = viewportElement.clientWidth
+  const height = viewportElement.clientHeight
+  if (canvas.width !== width || canvas.height !== height) {
+    // console.log("Canvas resized from ", canvas.width, canvas.height, " to ", width, height)
+    canvas.clientWidth = width
+    canvas.clientHeight = height
+    canvas.width = width
+    canvas.height = height
+
+    camera.aspect = (width / height)
+    camera.updateProjectionMatrix()
+    renderer.setSize(width, height)
+  }
 }
 
-// iterate through every dream and add it to our page
-dreams.forEach(function (dream) {
-  appendNewDream(dream)
-})
+// Update the geometry every frame
+function update () {
+  resize()
 
-// listen for the form to be submitted and add a new dream when it is
-dreamsForm.onsubmit = function (event) {
-  // stop our form submission from refreshing the page
-  event.preventDefault()
-
-  // get dream value and add it to the list
-  dreams.push(dreamInput.value)
-  appendNewDream(dreamInput.value)
-
-  // reset form
-  dreamInput.value = ''
-  dreamInput.focus()
+  mesh1.rotation.x += 0.01
+  mesh1.rotation.y += 0.01
 }
+
+// MAIN render function. This is what should requestAnimationFrame
+function render () {
+  update()
+  renderer.render(scene, camera)
+  window.requestAnimationFrame(render)
+}
+
+window.requestAnimationFrame(render)
--- server.js
+++ server.js
@@ -11,9 +11,13 @@ const express = require('express')
 const app = express()
 const helmet = require('helmet')
 const path = require('path')
+const compression = require('compression')
 
 // Helmet middleware
 app.use(helmet())
+
+// Compression middleware
+app.use(compression())
 
 // Host static files
 app.use(express.static('public'))
