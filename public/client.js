f03aca7a81d154e691453d0b7003fc52c6008528
Terry Hearst <thearst3rd@gmail.com>
2019-09-27 22:04:49 -0400

    Significant progress. All I gotta do is actually implement GOL

--- public/client.js
+++ public/client.js
@@ -10,17 +10,19 @@ const dat = require('dat.gui')
 // ## SETUP GAME OF LIFE ##
 // ########################
 
-const gridSize = 3
+const gridSize = 10
+
+function showHelp () {
+  const popup = document.getElementById('help-popup')
+  popup.classList.toggle('show')
+}
 
 const GameOfLifeClass = function () {
   this.running = false
   this.speed = 3
-  this.edit = function () {
-    window.alert('Button pressed')
-  }
-  this.showHelp = function () {
-    window.alert('Show help')
-  }
+  this.rotSpeed = 0.6
+  this.editMode = false
+  this.showHelp = showHelp
 }
 
 const gameOfLife = new GameOfLifeClass()
@@ -33,9 +35,13 @@ window.onload = function () {
   const gui = new dat.GUI()
 
   gui.add(gameOfLife, 'running').name('Running')
-  gui.add(gameOfLife, 'speed', 1, 20).name('Iterations/sec').step(1)
-  gui.add(gameOfLife, 'edit').name('Edit board contents')
-  gui.add(gameOfLife, 'showHelp').name('Show help')
+  gui.add(gameOfLife, 'speed', 1, 20).name('Iterations/Sec').step(1)
+  gui.add(gameOfLife, 'rotSpeed', 0, 5).name('Rotation Speed')
+  gui.add(gameOfLife, 'editMode').name('Edit Mode')
+  gui.add(gameOfLife, 'showHelp').name('Toggle Help')
+
+  // Show the help screen at the start
+  showHelp()
 }
 
 // ###########################
@@ -62,50 +68,175 @@ renderer.setSize(w, h)
 
 // Setup Scene and Camera objs
 const scene = new THREE.Scene()
-const camera = new THREE.PerspectiveCamera(75, w / h, 0.1, 1000)
+const camera = new THREE.PerspectiveCamera(60, w / h, 0.1, 1000)
 
-camera.position.z = 8
+camera.position.z = 11
 
-// Create different geometries
+// Create different geometries (and make them all the correct size)
 const geos =
 [
-  new THREE.Geometry(),   // Blank
-  new THREE.SphereGeometry(1.3/(gridSize/2), 8, 6),   // Sphere
-  new THREE.TorusGeometry(1.3/(gridSize/2), 0.5/(gridSize/2), 8, 16),   // Simple torus
-  new THREE.TorusKnotGeometry(1.3/(gridSize/2), 0.6/(gridSize/2), 64, 8, 2, 3),  // Torus knot 1
-  new THREE.TorusKnotGeometry(1.5/(gridSize/2), 0.45/(gridSize/2), 64, 8, 3, 4)  // Torus knot 2
+  /* Blank  */ new THREE.Geometry(),
+  /* Sphere */ new THREE.SphereGeometry(1.3 / (gridSize / 2), 8, 6),
+  /* Torus  */ new THREE.TorusGeometry(1.3 / (gridSize / 2), 0.5 / (gridSize / 2), 8, 16),
+  /* Knot 1 */ new THREE.TorusKnotGeometry(1.3 / (gridSize / 2), 0.5 / (gridSize / 2), 64, 8, 2, 3),
+  /* Knot 2 */ new THREE.TorusKnotGeometry(1.5 / (gridSize / 2), 0.45 / (gridSize / 2), 64, 8, 3, 4)
 ]
+
+// Create different materials
 const mats =
 [
   new THREE.MeshStandardMaterial({ color: 0xFF0000 }),
   new THREE.MeshStandardMaterial({ color: 0xFFFF00 }),
-  new THREE.MeshToonMaterial({ color: 0x30FF30 }),
-  new THREE.MeshNormalMaterial(/*{ color: 0x4040FF }*/),
+  new THREE.MeshStandardMaterial({ color: 0x30FF30 }),
+  new THREE.MeshNormalMaterial(/* { color: 0x4040FF } */)
 ]
 
-const meshes = []
-for (let i = 0; i < gridSize; i++) {
-  meshes[i] = []
-  for (let j = 0; j < gridSize; j++) {
-    meshes[i][j] = new THREE.Mesh(geos[0], mats[0])
+// HELPER - Maps a value from one range to another
+function map (x, low1, high1, low2, high2) {
+  const r1 = high1 - low1
+  const r2 = high2 - low2
+  return ((x - low1) * (r2 / r1)) + low2
+}
+
+// HELPER - Calculate x, y position of cell based on i, j index
+function calcCellPosition (i, j) {
+  return {
+    x: map(i, 0, gridSize - 1, 5 * (-(gridSize - 1) / gridSize), 5 * ((gridSize - 1) / gridSize)),
+    y: -map(j, 0, gridSize - 1, 5 * (-(gridSize - 1) / gridSize), 5 * ((gridSize - 1) / gridSize))
   }
 }
-const mesh1 = new THREE.Mesh(geos[4], mats[3])
 
-scene.add(mesh1)
+// Create a mesh for each cell
+const meshes = []
+for (let j = 0; j < gridSize; j++) {
+  meshes[j] = []
+  for (let i = 0; i < gridSize; i++) {
+    const rand1 = Math.floor(Math.random() * 5)
+
+    const mesh = new THREE.Mesh(geos[rand1], mats[rand1 >= 0 ? rand1 - 1 : 0])
+
+    // Position all meshes in the center of that mesh's square
+    const cellPosition = calcCellPosition(i, j)
+    mesh.position.x = cellPosition.x
+    mesh.position.y = cellPosition.y
+
+    const rotationOffset = Math.random() * Math.PI * 2
+    mesh.rotation.x = rotationOffset
+    mesh.rotation.y = rotationOffset
+
+    scene.add(mesh)
+
+    meshes[j][i] = mesh
+  }
+}
+
+// TEST - make mesh for the corners of the board
+/*
+const meshCornerTopLeft = new THREE.Mesh(geos[2], mats[3])
+const meshCornerBottomRight = new THREE.Mesh(geos[3], mats[3])
+
+meshCornerTopLeft.position.x = -5
+meshCornerTopLeft.position.y = 5
+meshCornerBottomRight.position.x = 5
+meshCornerBottomRight.position.y = -5
+
+scene.add(meshCornerTopLeft)
+scene.add(meshCornerBottomRight)
+*/
 
 // Define lighting
-const ambLight = new THREE.AmbientLight(0x404040)
-const dirLight = new THREE.DirectionalLight(0xFFFFFF)
-
+const ambLight = new THREE.AmbientLight(0x303030)
 scene.add(ambLight)
-scene.add(dirLight)
 
-// Grid
+const dirLight = new THREE.DirectionalLight(0xFFFFFF)
+const lightTarget = new THREE.Object3D()
+lightTarget.position.x = -5
+lightTarget.position.z = -5
+lightTarget.position.y = -5
+scene.add(lightTarget)
+scene.add(dirLight)
+dirLight.target = lightTarget
+
+// Make grid
 const gridColor = 0x808080
 const gridHelper = new THREE.GridHelper(10, gridSize, gridColor, gridColor)
 gridHelper.rotation.x = Math.PI / 2
 scene.add(gridHelper)
+
+// Make mesh for the object that will assist you in edit mode
+const s = 3 / (gridSize / 2)
+const editGeo = new THREE.CubeGeometry(s, s, s)
+const editMat = new THREE.MeshBasicMaterial({color: 0xFFFFFF, transparent: true, opacity: 0.3})
+
+const editMesh = new THREE.Mesh(editGeo, editMat)
+scene.add(editMesh)
+
+// ############################
+// ## HANDLE MOUSE MOVEMENTS ##
+// ############################
+
+// Get screen coords for the board
+// Taken from https://stackoverflow.com/questions/27409074/converting-3d-position-to-2d-screen-position-r69
+/*
+ * NOTE:
+ * I originally was trying to use the above solution which *apparently* you're supposed to be able to give it an object
+ * or position (there were variations) that would return the screen space coords. I tried all of them. All of them gave
+ * me positions of +/- infinity for both x and y. So I use this janky method that WILL WORK assuming that you don't
+ * change the camera's position. Whatever.
+ */
+const boardBoundaries = {x1: 0, y1: 0, x2: 0, y2: 0}
+function calculateBoardCornerScreenCoordinates () {
+  // uhh yeah, I just measured these...
+  // they work in all resolutions I promise... at least they're close enough
+  
+  const top = map(138, 0, 1287, 0, canvas.clientHeight)
+  const distFromCenter = (canvas.clientHeight / 2) - top
+  
+  boardBoundaries.x1 = Math.round((canvas.clientWidth / 2)  - distFromCenter)
+  boardBoundaries.y1 = Math.round((canvas.clientHeight / 2) - distFromCenter)
+
+  boardBoundaries.x2 = Math.round((canvas.clientWidth / 2)  + distFromCenter)  
+  boardBoundaries.y2 = Math.round((canvas.clientHeight / 2) + distFromCenter)
+
+  console.log('New board screen coordinates:')
+  console.log('x1: ', boardBoundaries.x1)
+  console.log('y1: ', boardBoundaries.y1)
+  console.log('x2: ', boardBoundaries.x2)
+  console.log('y2: ', boardBoundaries.y2)
+}
+
+calculateBoardCornerScreenCoordinates()
+
+window.onmousemove = function (event) {
+  if (gameOfLife.editMode) {
+    editMat.opacity = 0.3
+    
+    const i = Math.round(map(event.x, boardBoundaries.x1, boardBoundaries.x2, 0, gridSize))
+    const j = Math.round(map(event.y, boardBoundaries.y1, boardBoundaries.y2, 0, gridSize))
+    
+    //console.log(i, j)
+    
+    const cellPosition = calcCellPosition(i, j)
+    
+    editMesh.position.x = cellPosition.x
+    editMesh.position.y = cellPosition.y
+  } else {
+    editMat.opacity = 0
+  }
+}
+
+window.onmouseclick = function (event) {
+   if (gameOfLife.editMode) {
+    
+    const i = Math.round(map(event.x, boardBoundaries.x1, boardBoundaries.x2, 0, gridSize))
+    const j = Math.round(map(event.y, boardBoundaries.y1, boardBoundaries.y2, 0, gridSize))
+    
+    console.log("Clicked at ", i, j)
+    
+    // TODO
+    //gameOfLife.cells[j][i] = 
+  }
+}
 
 // ##############################
 // ## MAIN RENDERING FUNCTIONS ##
@@ -125,20 +256,43 @@ function resize () {
     camera.aspect = (width / height)
     camera.updateProjectionMatrix()
     renderer.setSize(width, height)
+    
+    calculateBoardCornerScreenCoordinates()
   }
 }
 
 // Update the geometry every frame
-function update () {
+// dt - time in seconds since last frame
+function update (dt) {
   resize()
 
-  mesh1.rotation.x += 0.01
-  mesh1.rotation.y += 0.01
+  // Rotate all meshes
+  for (let j = 0; j < gridSize; j++) {
+    for (let i = 0; i < gridSize; i++) {
+      const mesh = meshes[j][i]
+      mesh.rotation.x += (gameOfLife.rotSpeed * dt)
+      mesh.rotation.y += (gameOfLife.rotSpeed * dt)
+    }
+  }
 }
+
+// Get the timestamp from the previous frame to ensure consistent times even with different framerates
+// CODE TAKEN FROM:
+// https://codeincomplete.com/posts/javascript-game-foundations-the-game-loop/
+function timestamp () {
+  return window.performance && window.performance.now ? window.performance.now() : new Date().getTime()
+}
+let now, dt
+let last = timestamp()
 
 // MAIN render function. This is what should requestAnimationFrame
 function render () {
-  update()
+  now = timestamp()
+  dt = (now - last) / 1000 // to get time in seconds
+  if (dt > 1 / 20) dt = (1 / 20) // will slow down time if framerate dips below 20fps
+  last = now
+
+  update(dt)
   renderer.render(scene, camera)
   window.requestAnimationFrame(render)
 }
