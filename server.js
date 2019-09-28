920fcdbdbbd170c03fb1305caae12fb3453b4a7a
AlejandraGarza42 <agarza@wpi.edu>
2019-09-27 22:01:32 -0400

    cleaned up files

--- server.js
+++ server.js
@@ -1,22 +1,11 @@
-//import?
-//import * as THREE from 'three';
-
 const express       = require( 'express' ),
       app           = express(),
-     // session       = require( 'express-session' ),
       helmet        = require('helmet'),
       compression   = require('compression')
-      //THREE         = require('three')
-//       babelify = require("babelify");
-// browserify().transform(babelify, {presets: ["@babel/preset-env", "@babel/preset-react"]});
+
 app.use(express.static('./'))
 app.use(helmet())
 app.use(compression())
-
-
-//for three.js
-//app.use(express.static('THREE'))
-//var scene = new THREE.Scene() -- i think i dont need to do this here 
 
 //this was after passport
 app.use( require('express-session')({ secret:'cats cats cats', resave:false, saveUninitialized:false }));
