8a838ee87a099b108ebfa6bc0c34ccbd0ec21663
Hunter <huntercaouette@gmail.com>
2019-09-30 00:41:47 -0400

    drawing?

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -11,7 +11,6 @@ let setup = false //indicate if audio is set up yet
 
 function submit( e ) {
   console.log('button pressed')
-
       // Create an AudioContext for output
       if (audioContext) {
         audioContext.close()
@@ -19,7 +18,6 @@ function submit( e ) {
       }
       audioContext = new AudioContext()
       bufferSource = audioContext.createBufferSource()
-
       // Open an XMLHttpRequest to stream audio data from YouTube
       const videoUrl = document.querySelector( '#ytvid' ).value;
       let videoId = ''
@@ -30,11 +28,9 @@ function submit( e ) {
         console.log('failed to split video url. try again.')
         return
       }
-
       const request = new XMLHttpRequest()
       request.open('POST', `/stream/${videoId}`, true)
       request.responseType = 'arraybuffer'
-
       request.onload = function() {
         console.log('starting audio decode')
         // Decode the arraybuffer from the XMLHttpRequest
@@ -42,13 +38,11 @@ function submit( e ) {
           // Connect the audio buffer to the AudioContext for output
           bufferSource.buffer = buffer
           play()
-
         }, error => {
           alert('Unable to process audio stream')
           console.error(error)
         })
       }
-
       request.send()
     return false
 }
@@ -57,14 +51,39 @@ function play(){
   if (!audioContext) {
     return 0
   }
-  // var analyser = audioContext.createAnalyser()
-  // source = audioContext.createMediaStreamSource(stream);
-  // source.connect(analyser);
-  // analyser.connect(distortion);
-  // distortion.connect(audioContext.destination);
+  //create fft
+  fft = audioContext.createAnalyser();
+  fft.fftSize = samples;
 
-  bufferSource.connect(audioContext.destination)
+  //connect them up into a chain
+  bufferSource.connect(fft);
+  fft.connect(audioContext.destination);
+
   bufferSource.start(0)
+  setup = true
+}
+
+var gfx;
+function setupCanvas() {
+    var canvas = document.getElementById('canvas');
+    gfx = canvas.getContext('2d');
+    webkitRequestAnimationFrame(update);
+}
+
+function update() {
+    webkitRequestAnimationFrame(update);
+    if(!setup) return;
+    gfx.clearRect(0,0,800,600);
+    gfx.fillStyle = 'gray';
+    gfx.fillRect(0,0,800,600);
+
+    var data = new Uint8Array(samples);
+    fft.getByteFrequencyData(data);
+    gfx.fillStyle = 'red';
+    for(var i=0; i<data.length; i++) {
+        gfx.fillRect(100+i*4,100+256-data[i]*2,3,100);
+    }
+
 }
 
 
