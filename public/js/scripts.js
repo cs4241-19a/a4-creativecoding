7903bac140ac98fdf5a8c3b20cce4b8731eac63c
Hunter <huntercaouette@gmail.com>
2019-09-29 20:09:30 -0400

    submit yt might work now

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -2,6 +2,62 @@
 
 console.log("Welcome to assignment 3!")
 
+
+
+$(document).ready(() => {
+  let audioContext = null
+  let audioIntervalId = null
+
+  $('.video-input-form').submit(() => {
+
+      // Create an AudioContext for output
+      if (audioContext) {
+        audioContext.close()
+        clearInterval(audioIntervalId)
+      }
+      audioContext = new AudioContext()
+      const audioContextTimestamp = Date.now()
+      const bufferSource = audioContext.createBufferSource()
+      const resonanceAudio = new ResonanceAudio(audioContext)
+      resonanceAudio.output.connect(audioContext.destination)
+
+      // Open an XMLHttpRequest to stream audio data from YouTube
+      const videoUrl = $('.ytvid').val()
+      let videoId = ''
+      try {
+        videoId = videoUrl.split('=')[1]
+      } catch (error) {
+      // eslint-disable-next-line no-console
+        console.log('failed to split video url. try again.')
+        return
+      }
+      const request = new XMLHttpRequest()
+      request.open('POST', `/stream/${videoId}`, true)
+      request.responseType = 'arraybuffer'
+      request.onload = function() {
+
+        // Decode the arraybuffer from the XMLHttpRequest
+        audioContext.decodeAudioData(request.response, buffer => {
+
+          // Connect the audio buffer to the AudioContext for output
+          const source = resonanceAudio.createSource()
+          bufferSource.buffer = buffer
+          bufferSource.connect(source.input)
+          bufferSource.start()
+          const audioContextDelay = Date.now() - audioContextTimestamp
+
+        }, error => {
+          alert('Unable to process audio stream')
+          console.error(error)
+        })
+      }
+      request.send()
+    })
+    return false
+})
+
+
+
     // Hacks to deal with different function names in different browsers
     window.requestAnimFrame = (function(){
       return  window.requestAnimationFrame       ||
