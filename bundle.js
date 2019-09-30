7e73694f9c3b775b93f25cd6fdab5c353d458aa4
Hunter <huntercaouette@gmail.com>
2019-09-30 00:31:08 -0400

    and the bundle, too

--- bundle.js
+++ bundle.js
@@ -2544,7 +2544,7 @@ console.log("Welcome to assignment 3!")
 
 let audioContext = null
 let audioIntervalId = null
-const bufferSource = null
+let bufferSource = null
 let fft = null //fft audio node
 let samples = 128
 let setup = false //indicate if audio is set up yet
