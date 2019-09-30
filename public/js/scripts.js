e44beb434c3a47bb1c1772baeea15697b545f26b
Hunter <huntercaouette@gmail.com>
2019-09-29 20:25:01 -0400

    script runs maybe

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -2,13 +2,38 @@
 
 console.log("Welcome to assignment 3!")
 
+// const submit = function( e ) {
+// // prevent default form action from being carried out
+// e.preventDefault()
+//
+// const video = document.querySelector( '#ytvid' ),
+//       json = { url: video.value },
+//       body = JSON.stringify( json )
+//
+//     fetch( '/submit', {
+//     method:'POST',
+//     body
+//   })
+//   .then( function( response ) {
+//     // do something with the reponse
+//     console.log( response )
+//     response.text().then(function(text){
+//       console.log(text)
+//       setCookie(inputName.value, text, 1);
+//       let jOBJ = JSON.parse(text)
+//       alert("congrats! you are a "+jOBJ['horoscope']+" in the year of the "+jOBJ['zodiac']+"!")
+//       //cookie is set to the table value stored in the temporary server, now cached for a day
+//     })
+//   })
+// }
 
-
-$(document).ready(() => {
+window.onload = function() {
   let audioContext = null
   let audioIntervalId = null
 
-  $('.video-input-form').submit(() => {
+  const submit = function( e ) {
+
+    console.log('submitting video url')
 
       // Create an AudioContext for output
       if (audioContext) {
@@ -22,7 +47,7 @@ $(document).ready(() => {
       resonanceAudio.output.connect(audioContext.destination)
 
       // Open an XMLHttpRequest to stream audio data from YouTube
-      const videoUrl = $('.ytvid').val()
+      const videoUrl = document.querySelector( '#ytvid' ).value();
       let videoId = ''
       try {
         videoId = videoUrl.split('=')[1]
@@ -31,6 +56,7 @@ $(document).ready(() => {
         console.log('failed to split video url. try again.')
         return
       }
+      console.log(videoId)
       const request = new XMLHttpRequest()
       request.open('POST', `/stream/${videoId}`, true)
       request.responseType = 'arraybuffer'
@@ -55,123 +81,3 @@ $(document).ready(() => {
     })
     return false
 })
-
-
-
-    // Hacks to deal with different function names in different browsers
-    window.requestAnimFrame = (function(){
-      return  window.requestAnimationFrame       ||
-              window.webkitRequestAnimationFrame ||
-              window.mozRequestAnimationFrame    ||
-              function(callback, element){
-                window.setTimeout(callback, 1000 / 60);
-              };
-    })();
-    window.AudioContext = (function(){
-        return  window.webkitAudioContext || window.AudioContext || window.mozAudioContext;
-    })();
-    // Global Variables for Audio
-    var audioContext;
-    var audioBuffer;
-    var sourceNode;
-    var analyserNode;
-    var javascriptNode;
-    var audioData = null;
-    var audioPlaying = false;
-    var sampleSize = 1024;  // number of samples to collect before analyzing data
-    var amplitudeArray;     // array to hold time domain data
-    // This must be hosted on the same server as this page - otherwise you get a Cross Site Scripting error
-    var audioUrl = "viper.mp3";
-    // Global Variables for the Graphics
-    var canvasWidth  = 512;
-    var canvasHeight = 256;
-    var ctx;
-    $(document).ready(function() {
-        ctx = $("#canvas").get()[0].getContext("2d");
-        // the AudioContext is the primary 'container' for all your audio node objects
-        try {
-            audioContext = new AudioContext();
-        } catch(e) {
-            alert('Web Audio API is not supported in this browser');
-        }
-        // When the Start button is clicked, finish setting up the audio nodes, play the sound,
-        // gather samples for the analysis, update the canvas
-        $("#start_button").on('click', function(e) {
-            e.preventDefault();
-            // Set up the audio Analyser, the Source Buffer and javascriptNode
-            setupAudioNodes();
-            // setup the event handler that is triggered every time enough samples have been collected
-            // trigger the audio analysis and draw the results
-            javascriptNode.onaudioprocess = function () {
-                // get the Time Domain data for this sample
-                analyserNode.getByteTimeDomainData(amplitudeArray);
-                // draw the display if the audio is playing
-                if (audioPlaying == true) {
-                    requestAnimFrame(drawTimeDomain);
-                }
-            }
-            // Load the Audio the first time through, otherwise play it from the buffer
-            if(audioData == null) {
-                loadSound(audioUrl);
-            } else {
-                playSound(audioData);
-            }
-        });
-        // Stop the audio playing
-        $("#stop_button").on('click', function(e) {
-            e.preventDefault();
-            sourceNode.stop(0);
-            audioPlaying = false;
-        });
-    });
-    function setupAudioNodes() {
-        sourceNode     = audioContext.createBufferSource();
-        analyserNode   = audioContext.createAnalyser();
-        javascriptNode = audioContext.createScriptProcessor(sampleSize, 1, 1);
-        // Create the array for the data values
-        amplitudeArray = new Uint8Array(analyserNode.frequencyBinCount);
-        // Now connect the nodes together
-        sourceNode.connect(audioContext.destination);
-        sourceNode.connect(analyserNode);
-        analyserNode.connect(javascriptNode);
-        javascriptNode.connect(audioContext.destination);
-    }
-    // Load the audio from the URL via Ajax and store it in global variable audioData
-    // Note that the audio load is asynchronous
-    function loadSound(url) {
-        document.getElementById('msg').textContent = "Loading audio...";
-        var request = new XMLHttpRequest();
-        request.open('GET', url, true);
-        request.responseType = 'arraybuffer';
-        // When loaded, decode the data and play the sound
-        request.onload = function () {
-            audioContext.decodeAudioData(request.response, function (buffer) {
-                document.getElementById('msg').textContent = "Audio sample download finished";
-                audioData = buffer;
-                playSound(audioData);
-            }, onError);
-        }
-        request.send();
-    }
-    // Play the audio and loop until stopped
-    function playSound(buffer) {
-        sourceNode.buffer = buffer;
-        sourceNode.start(0);    // Play the sound now
-        sourceNode.loop = true;
-        audioPlaying = true;
-    }
-    function onError(e) {
-        console.log(e);
-    }
-    function drawTimeDomain() {
-        clearCanvas();
-        for (var i = 0; i < amplitudeArray.length; i++) {
-            var value = amplitudeArray[i] / 256;
-            var y = canvasHeight - (canvasHeight * value) - 1;
-            ctx.fillStyle = '#ffffff';
-            ctx.fillRect(i, y, 1, 1);
-        }
-    }
-    function clearCanvas() {
-        ctx.clearRect(0, 0, canvasWidth, canvasHeight);
-    }
